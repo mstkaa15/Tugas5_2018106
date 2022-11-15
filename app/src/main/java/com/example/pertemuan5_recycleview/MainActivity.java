23f22040a0bea14891a2cb615d59ab49460d188c
mstkaa15 <mustikaaisyah18@gmail.com>
2022-11-15 20:44:22 +0700

    RecyclerView

---
+++ app/src/main/java/com/example/pertemuan5_recycleview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pertemuan5_recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recylerView;
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.cermin,R.drawable.kursi,R.drawable.meja,R.drawable.sofa};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.makanan);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        MakananAdapter appAdapter = new MakananAdapter(this,s1,s2,s3,images);
+        recylerView.setAdapter(appAdapter);
+        recylerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+    }
+}
